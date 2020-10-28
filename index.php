3105d4e14aca4f66ad4fca5285160b706fa115e8
Frank Martin Moreno Alburqueque <admin@frankmorenoalburqueque.com>
2020-10-28 14:54:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,24 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row mt-4 pt-4">
+				<div class="col-sm-4">
+					<div class="dropdown-menu">
+						<span class="dropdown-item-text">Base 64</span>
+						<a class="dropdown-item" href="#">Encode</a>
+						<a class="dropdown-item" href="#">Decode</a>
+						<a class="dropdown-item" href="#">Encode HTML</a>
+						<a class="dropdown-item" href="#">Decode HTML</a>
+					</div>
+				</div>
+				<div class="col-sm-8">
+					
+				</div>
+			</div>
+		</div>
+	</div>
 
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
